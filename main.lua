7096a9150adbd44dd9f7ea830d8eadd0b48371dd
Michael Mason <TeaUponTweed@gmail.com>
2017-02-23 20:55:19 -0700

    wip

--- main.lua
+++ main.lua
@@ -2,8 +2,12 @@ require "autobatch"
 
 local camera = require("camera")
 local ecs = require("ecs")
+local vec = require("vector")
 -- local kdtree = require("kdtree")
-
+-- local QuadTree = require("quadtree").QuadTree
+min = math.min
+max = math.max
+MAX_SPEED = 20
 
 function love.load()
   love.window.setMode(0, 0, {resizable=true})
@@ -54,8 +58,45 @@ function love.load()
 
 
 function love.update(dt)
+    -- for _, p in pairs(ecs.components.position) do
+    --     p.width = 10
+    --     p.height = 10
+    --     qtree:addObject(p)
+    -- end
+    -- function intersects(( ... )
+        -- body
+    -- end
+    -- function  contains(p1, p2)
+    --   if p1 == p2 then
+    --     return false
+    --   end
+    --   return min(math.abs(p1.x + p1.width/2 - (p2.x + p2.width/2)),
+    --              math.abs(p1.y + p1.height/2 - (p2.y + p2.height/2))) <= (min(p1.width, p1.height) + min(p2.width, p2.height))
+
+    --     -- if p1.x < p2.x and p2.x <= p1.x + p1.width
+    --     -- return p1.x <= p2.x and p2.x <= p1.x + p1.width and p1.y <= p2.y and p2.y <= p1.y + p1.height
+    -- end
+    -- function distance(p1, p2)
+    --   dx = p1.x - p2.x
+    --   dy = p1.y - p2.y
+    --   return math.sqrt(dx*dx + dy*dy)
+    -- end
     ecs:process({'position', 'velocity'},
                 function (p, v)
+                    for _, otherp in pairs(ecs.components.position) do
+                    -- for _, otherp in pairs(qtree:getCollidableObjects(p, false)) do
+                        local dx = p.x - otherp.x
+                        local dy = p.y - otherp.y
+                        local distance = math.sqrt(dx*dx + dy*dy)
+                        if p ~= otherp and distance < 10 then
+                            v.vx = -v.vx
+                            v.vy = -v.vy
+                            p.x = otherp.x + (10/distance * dx)
+                            p.y = otherp.y + (10/distance * dy)
+                            -- dx = p1.x - p2.x
+                            break
+                        end
+                    end
                     p.x = p.x + v.vx * dt
                     p.y = p.y + v.vy * dt
                 end)
@@ -68,43 +109,71 @@ function love.update(dt)
 end
 
 function love.draw() -- TODO use kd tree to find visible objects, requires ecs extension based on position
+    local function my_bounds(o, min, max)
+        min[1], min[2] = o.x, o.y
+        max[1], max[2] = o.x+10, o.y+10
+        return min, max
+    end
+
+    -- local tree = kdtree.build(my_bounds, 2, ecs.components.position)
+    -- unpack(camera:abs(mousex, mousey))
+    -- selected = {}
+    -- print(ecs.components.position)
+    -- for o in tree:query(camera:abs(unpack(MOUSE_1_DOWN)), camera:abs(unpack(MOUSE_1_BOX))) do
+        -- selected[o] = true
+  -- print("Found", tostring(o))
+    -- end
+    -- box_edges
+
     love.graphics.push()
     love.graphics.scale(1.0/camera.scale, 1.0/camera.scale)
     love.graphics.translate(-camera.x, -camera.y)
     ecs:process({'position', 'drawer'},
                 function (p, d)
-                  d.draw(p.x, p.y)
+                    if MOUSE_1_BOX and MOUSE_1_DOWN then
+                        local absd = camera:abs(unpack(MOUSE_1_DOWN))
+                        local absb = camera:abs(unpack(MOUSE_1_BOX))
+                        local t, r, b, l = min(absd[2], absb[2]), max(absd[1], absb[1]), max(absd[2], absb[2]), min(absd[1], absb[1])
+                        selected = (l <= p.x and
+                                    t <= p.y and
+                                    r >= p.x and
+                                    b >= p.y)
+                        if selected and love.mouse.isDown(1) then
+                            love.graphics.setColor(255, 0, 0)
+                        else
+                            love.graphics.setColor(255, 255, 255)
+                        end
+                    end
+                    d.draw(p.x, p.y)
                 end )
     love.graphics.pop()
     love.graphics.print(tostring(love.timer.getFPS( )), 10, 10)
+    -- if love.mouse.isDown(1) then
+        -- love.graphics.print(string.format('TL: (%s, %s), BR: (%s, %s)', MOUSE_1_DOWN[1], MOUSE_1_DOWN[2], MOUSE_1_BOX[1], MOUSE_1_BOX[2]), 10, 40)
+        -- love.graphics.print(string.format('TL: (%s, %s), BR: (%s, %s)', TL[1], TL[2], BR[1], BR[2]), 10, 80)
+    -- end
 end
 
+-- MOUSE_1_DOWN = {0, 0}
 
 function love.mousepressed(px, py, button, istouch)
     -- TODO serialize input
-    if button == 2 then
-      for _=1,2000 do
-        local isbear = love.math.random() <= .2
-        local draw_func = function(x, y)
-                            if isbear then
-                                love.graphics.setColor(150, 75, 0)
-                            else
-                                love.graphics.setColor(255, 255, 255)
-                            end
-                            love.graphics.rectangle("fill", x, y, 10, 10)
-                    end
-        ecs:new_entity():with('position', {unpack(camera:abs(px, py))}):
-                         with('velocity', {20*(love.math.random()-.5), 20*(love.math.random()-.5)}):
-                         with('drawer', {draw_func})
-     end
-   end
+    if button == 1 then
+        MOUSE_1_DOWN = {px, py}
+        MOUSE_1_BOX = nil
+    end
 end
 
 
+MOUSE_1_BOX = {0, 0}
 function love.mousemoved( px, py, pdx, pdy, istouch )
+    mousex, mousey = px, py
     if love.mouse.isDown(1) then
         camera:move_pixels(-pdx, -pdy)
     end
+    -- if love.mouse.isDown(1) then
+    --     MOUSE_1_BOX = {px, py}
+    -- end
 end
 
 
@@ -121,6 +190,28 @@ end
 function love.keypressed(k)
    if k == 'escape' then
       love.event.quit()
+   elseif k == 'space' then
+      for _=1,20 do
+        local isbear = love.math.random() <= .2
+        local draw_func = function(x, y)
+                            -- if isbear then
+                            --     love.graphics.setColor(150, 75, 0)
+                            -- else
+                            --     love.graphics.setColor(255, 255, 255)
+                            -- end
+                            love.graphics.rectangle("fill", x, y, 10, 10)
+                    end
+        dir = love.math.random() * 2 * math.pi
+        speed = love.math.random() * 100
+        vx = speed*math.cos(dir)
+        vy = speed*math.sin(dir)
+        px, py = unpack(camera:abs(mousex, mousey))
+        px = px + (love.math.random()-.5)*200
+        py = py + (love.math.random()-.5)*200
+        ecs:new_entity():with('position', {px, py}):
+                         with('velocity', {vx, vy}):
+                         with('drawer', {draw_func})
+     end
    end
 end
 
@@ -129,3 +220,5 @@ function love.resize(w, h)
   camera.pix_w = w
   camera.pix_h = h
 end
+
+
