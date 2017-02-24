90eb228fc461837d7d4bac275784081ddcce87a6
Michael Mason <TeaUponTweed@gmail.com>
2017-02-23 23:42:51 -0700

    clean up quadtree and fix mysterious metaclass bug

--- quadtree.lua
+++ quadtree.lua
@@ -1,50 +1,48 @@
-QuadTree = {}
+local QuadTree = {}
+
+QuadTree.__index = QuadTree
+
 QuadTree.MAX_OBJECTS=5
 
-
 function QuadTree.new(left, top, width, height)
-    o = {}
-    setmetatable(o, self)
+    local self = setmetatable({}, QuadTree)
     self.x = left
     self.y = top
     self.w = width
     self.h = height
     self.children = nil
     self.objects = {}
-    return o
+    return self
 end
 
-
-function contains(qt, x, y)
-    return (x >= qt.x and qt.x + qt.width > x) and (y >= qt.y and qt.y + qt.height > y)
+function QuadTree:contains(x, y)
+    return (x >= self.x and self.x + self.w > x) and (y >= self.y and self.y + self.h > y)
 end
 
-
-function intersects(qt1, qt2)
-    return (contains(qt1, qt2.x        , qt2.y         ) or
-            contains(qt1, qt2.x + qt2.w, qt2.y         ) or
-            contains(qt1, qt2.x        , qt2.y + qt2.h ) or
-            contains(qt1, qt2.x + qt2.w, qt2.y + qt2.h ) )
+function QuadTree:intersects(qt)
+    return (self:contains(qt.x       , qt.y        ) or
+            self:contains(qt.x + qt.w, qt.y        ) or
+            self:contains(qt.x       , qt.y + qt.h ) or
+            self:contains(qt.x + qt.w, qt.y + qt.h ) )
 end
-
 
 function QuadTree:subdivide()
     if self.children then
-        for i,child in pairs(self.children) do
+        for _,child in pairs(self.children) do
             child:subdivide()
         end
     else
         local x = self.x
         local y = self.y
-        local w = self.width / 2
-        local h = self.height / 2
+        local w = self.w / 2
+        local h = self.h / 2
         self.children = {
             QuadTree.new(x    , y    , w, h),
             QuadTree.new(x + w, y    , w, h),
             QuadTree.new(x    , y + h, w, h),
             QuadTree.new(x + w, y + h, w, h)
         }
-        objects = self.objects
+        local objects = self.objects
         self.objects = nil
         for _, o in pairs(objects) do
             self:add(o)
@@ -52,24 +50,27 @@ function QuadTree:subdivide()
     end
 end
 
-
 function QuadTree:applyToLeaf(func)
     if self.children then
+        assert(not self.objects)
         for _, child in pairs(self.children) do
             child:applyToLeaf(func)
         end
     else
-        leafFunc(self)
+        assert(self.objects)
+        func(self)
     end
 end
 
 
 function QuadTree:add(o)
-    assert(contains(self, o.x, o.y))
+    -- assert(contains(self, o.x, o.y), self.x .. " " .. self.y .. " " .. self.w .. " " .. self.h)
     self:applyToLeaf(
         function (qt)
-            if contains(qt, o.x, o.y) then
-                qt.objects[o] = o
+            if qt:contains(o.x, o.y) then
+                -- print('ere')
+                qt.objects[#qt.objects+1] = o
+                -- print(#qt.objects)
                 if #qt.objects > QuadTree.MAX_OBJECTS then
                     qt:subdivide()
                 end
@@ -77,32 +78,29 @@ function QuadTree:add(o)
         end)
 end
 
-
-function QuadTree:remove(o) -- TODO collapse tree as objects are removed
+function QuadTree:remove(o) -- TODO collapse tree as objects are removed?
     self:applyToLeaf(
         function (qt)
             qt.objects[o] = nil
         end)
 end
 
-
 function QuadTree:update(o)
     self:remove(o)
     self:add(o)
 end
 
-
 function QuadTree:getIn(r)
-    ret = {}
+    local ret = {}
     self:applyToLeaf(
         function (qt)
-            if intersect(qt, r) then
+            if qt:intersects(r) then
                 for _, o in qt.objects do
                     ret[#ret+1] = o
                 end
             end
         end)
+    return ret
 end
-
 
 return QuadTree
