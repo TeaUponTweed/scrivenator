dda70b6bb59006e40f3f96275f5ffcaf969e00ea
Michael Mason <TeaUponTweed@gmail.com>
2017-03-10 20:11:15 -0700

    Initial commit with ggez helloworld

---
+++ src/quadtree.rs
@@ -0,0 +1,174 @@
+use sdl2::rect::Rect;
+
+const MAX_OBJECTS: usize = 15;
+const MAX_LEVELS: i32 = 20;
+
+pub trait HasExtent {
+    fn rect(&self) -> Rect;
+}
+
+/// A quadtree for minimizing collision checks between actors
+pub struct Quadtree<Type>  where Type: HasExtent {
+    /// The level of the current tree, (0 is root)
+    level: i32,
+    /// The actors that the current tree holds
+    objects: Vec<Type>,
+    /// An array of 4 subtrees to split into when parent is full
+    nodes: [Option<Box<Quadtree<Type>>>; 4],
+    /// The bounds of the current tree
+    bounds: Rect,
+}
+
+impl<Type> Quadtree<Type> where Type: HasExtent {
+    pub fn new(rect: Rect) -> Quadtree<Type> {
+        Quadtree {
+            level: 0,
+            objects: Vec::with_capacity(MAX_OBJECTS),
+            bounds: rect,
+            nodes: [None, None, None, None],
+        }
+    }
+
+    /// Splits the node into four subnodes
+    fn split(&mut self) {
+        let width = ((self.bounds.width() as f64) / 2.0) as i32;
+        let height = ((self.bounds.height() as f64) / 2.0) as i32;
+        let (x, y) = (self.bounds.x(), self.bounds.y());
+
+        if width as u32 > 0u32 && height as u32 > 0u32 {
+            self.nodes[0] = Some(Box::new(Quadtree {
+                level: self.level + 1,
+                objects: Vec::with_capacity(MAX_OBJECTS),
+                bounds: Rect::new(x + width, y, width as u32, height as u32),
+                nodes: [None, None, None, None],
+            }));
+            self.nodes[1] = Some(Box::new(Quadtree {
+                level: self.level + 1,
+                objects: Vec::with_capacity(MAX_OBJECTS),
+                bounds: Rect::new(x, y, width as u32, height as u32),
+                nodes: [None, None, None, None],
+            }));
+            self.nodes[2] = Some(Box::new(Quadtree {
+                level: self.level + 1,
+                objects: Vec::with_capacity(MAX_OBJECTS),
+                bounds: Rect::new(x, y + height, width as u32, height as u32),
+                nodes: [None, None, None, None],
+            }));
+            self.nodes[3] = Some(Box::new(Quadtree {
+                level: self.level + 1,
+                objects: Vec::with_capacity(MAX_OBJECTS),
+                bounds: Rect::new(x + width, y + height, width as u32, height as u32),
+                nodes: [None, None, None, None],
+            }));
+        }
+    }
+
+    /// Determine which node index the object belongs to
+    fn index(&self, rect: &Rect) -> Option<i32> {
+        let vert_mid = (self.bounds.x() as f64) + (self.bounds.width() as f64) / 2.;
+        let horiz_mid = (self.bounds.y() as f64) + (self.bounds.height() as f64) / 2.;
+
+        let top_quad = (rect.y() as f64) < horiz_mid &&
+                       (rect.y() as f64) + (rect.height() as f64) < horiz_mid;
+        let bot_quad = (rect.y() as f64) > horiz_mid;
+
+        if (rect.x() as f64) < vert_mid &&
+           (rect.x() as f64) + (rect.width() as f64) < vert_mid {
+            if top_quad {
+                return Some(1);
+            } else if bot_quad {
+                return Some(2);
+            }
+        } else if (rect.x() as f64) > vert_mid {
+            if top_quad {
+                return Some(0);
+            } else if bot_quad {
+                return Some(3);
+            }
+        }
+
+        None
+    }
+
+    /// Inserts an actor into the quadtree
+    pub fn insert(&mut self, actor: Type) where Type : HasExtent {
+        if self.nodes[0].is_some() {
+            if let Some(index) = self.index(&actor.rect()) {
+                if let Some(ref mut node) = self.nodes[index as usize] {
+                    node.insert(actor);
+                }
+                return;
+            }
+        }
+
+        if self.objects.len() == MAX_OBJECTS && self.level < MAX_LEVELS {
+            if self.nodes[0].is_none() {
+                self.split();
+            }
+
+            let mut leftover_parent = Vec::with_capacity(MAX_OBJECTS);
+            while !self.objects.is_empty() {
+                let object = self.objects.pop().unwrap();
+                if let Some(index) = self.index(&object.rect()) {
+                    if let Some(ref mut node) = self.nodes[index as usize] {
+                        node.insert(object);
+                    }
+                } else {
+                    leftover_parent.push(object);
+                }
+            }
+
+            // Handle the overflowing actor also
+            if let Some(index) = self.index(&actor.rect()) {
+                if let Some(ref mut node) = self.nodes[index as usize] {
+                    node.insert(actor);
+                }
+            } else {
+                leftover_parent.push(actor);
+            }
+
+            self.objects = leftover_parent;
+        } else {
+            self.objects.push(actor);
+        }
+    }
+
+    /// Return all objects that could collide
+    pub fn retrieve(&mut self, rect: &Rect) -> Vec<&Type> where Type: HasExtent {
+        let mut retrieved_values = Vec::new();
+        if let Some(index) = self.index(rect) {
+            if let Some(ref mut node) = self.nodes[index as usize] {
+                retrieved_values.extend(node.retrieve(rect).into_iter());
+            }
+        } else {
+            // if current object is not in a quadrant add all of the children
+            // since it could potentially collide with other objects in a quadrant
+            for node in &mut self.nodes[..] {
+                if let Some(ref mut node) = *node {
+                    retrieved_values.extend(node.retrieve(rect).into_iter());
+                }
+            }
+        }
+
+        for object in &self.objects {
+            if object.rect() != *rect {
+                retrieved_values.push(object);
+            }
+        }
+
+        retrieved_values
+    }
+
+    /// Returns the total number of elements in the quadtree
+    pub fn len(&self) -> usize {
+        let mut l = self.objects.len();
+
+        for i in 0..4 {
+            if let Some(ref node) = self.nodes[i] {
+                l += node.len();
+            }
+        }
+
+        l
+    }
+}
---
+++ src/vector.rs
@@ -0,0 +1,310 @@
+
+use std::ops::{Add, Div, Mul, Sub};
+
+/// A two-dimensional vector
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Vector2D {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Vector2D {
+    /// Returns the length of a vector
+    pub fn len(&self) -> f64 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    /// Returns a normalized vector
+    pub fn normalize(&self) -> Vector2D {
+        Vector2D {
+            x: self.x / self.len(),
+            y: self.y / self.len(),
+        }
+    }
+}
+
+impl Add<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn add(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Add<f64> for Vector2D {
+    type Output = Vector2D;
+
+    fn add(self, other: f64) -> Vector2D {
+        Vector2D {
+            x: self.x + other,
+            y: self.y + other,
+        }
+    }
+}
+
+impl Add<Vector2D> for f64 {
+    type Output = Vector2D;
+
+    fn add(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self + other.x,
+            y: self + other.y,
+        }
+    }
+}
+
+impl Sub<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn sub(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl Sub<f64> for Vector2D {
+    type Output = Vector2D;
+
+    fn sub(self, other: f64) -> Vector2D {
+        Vector2D {
+            x: self.x - other,
+            y: self.y - other,
+        }
+    }
+}
+
+impl Sub<Vector2D> for f64 {
+    type Output = Vector2D;
+
+    fn sub(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self - other.x,
+            y: self - other.y,
+        }
+    }
+}
+
+impl Mul<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn mul(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        }
+    }
+}
+
+impl Mul<f64> for Vector2D {
+    type Output = Vector2D;
+
+    fn mul(self, other: f64) -> Vector2D {
+        Vector2D {
+            x: self.x * other,
+            y: self.y * other,
+        }
+    }
+}
+
+impl Mul<Vector2D> for f64 {
+    type Output = Vector2D;
+
+    fn mul(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self * other.x,
+            y: self * other.y,
+        }
+    }
+}
+
+impl Div<Vector2D> for Vector2D {
+    type Output = Vector2D;
+
+    fn div(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self.x / other.x,
+            y: self.y / other.y,
+        }
+    }
+}
+
+impl Div<f64> for Vector2D {
+    type Output = Vector2D;
+
+    fn div(self, other: f64) -> Vector2D {
+        Vector2D {
+            x: self.x / other,
+            y: self.y / other,
+        }
+    }
+}
+
+impl Div<Vector2D> for f64 {
+    type Output = Vector2D;
+
+    fn div(self, other: Vector2D) -> Vector2D {
+        Vector2D {
+            x: self / other.x,
+            y: self / other.y,
+        }
+    }
+}
+
+
+/// Represents a change of position
+#[derive(Clone, Debug, PartialEq)]
+pub struct PositionChange {
+    pub x: i32,
+    pub y: i32,
+    pub w: i32,
+    pub h: i32,
+}
+
+impl PositionChange {
+    pub fn new() -> PositionChange {
+        PositionChange {
+            x: 0,
+            y: 0,
+            w: 0,
+            h: 0,
+        }
+    }
+
+    pub fn from_vector(v: &Vector2D) -> PositionChange {
+        PositionChange {
+            x: v.x as i32,
+            y: v.y as i32,
+            w: 0,
+            h: 0,
+        }
+    }
+
+    pub fn left(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x - amount,
+            y: self.y,
+            w: self.w,
+            h: self.h,
+        }
+    }
+
+    pub fn right(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x + amount,
+            y: self.y,
+            w: self.w,
+            h: self.h,
+        }
+    }
+
+    pub fn up(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y - amount,
+            w: self.w,
+            h: self.h,
+        }
+    }
+
+    pub fn down(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y + amount,
+            w: self.w,
+            h: self.h,
+        }
+    }
+
+    pub fn shrink_height_top(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y + amount,
+            w: self.w,
+            h: self.h - amount,
+        }
+    }
+
+    pub fn grow_height_top(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y - amount,
+            w: self.w,
+            h: self.h + amount,
+        }
+    }
+
+    pub fn shrink_height_bot(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y,
+            w: self.w,
+            h: self.h - amount,
+        }
+    }
+
+    pub fn grow_height_bot(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y,
+            w: self.w,
+            h: self.h + amount,
+        }
+    }
+
+    pub fn shrink_width_left(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x - amount,
+            y: self.y,
+            w: self.w - amount,
+            h: self.h,
+        }
+    }
+
+    pub fn grow_width_left(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x + amount,
+            y: self.y,
+            w: self.w + amount,
+            h: self.h,
+        }
+    }
+
+    pub fn shrink_width_right(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y,
+            w: self.w - amount,
+            h: self.h,
+        }
+    }
+
+    pub fn grow_width_right(&self, amount: i32) -> PositionChange {
+        PositionChange {
+            x: self.x,
+            y: self.y,
+            w: self.w + amount,
+            h: self.h,
+        }
+    }
+
+    pub fn chain(&self, change: &PositionChange) -> PositionChange {
+        PositionChange {
+            x: self.x + change.x,
+            y: self.y + change.y,
+            w: self.w + change.w,
+            h: self.h + change.h,
+        }
+    }
+
+    pub fn to_vector(&self) -> Vector2D {
+        Vector2D {
+            x: self.x as f64,
+            y: self.y as f64,
+        }
+    }
+}
