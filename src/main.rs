225a9d2c5706909d4723b49fde60f79af3406968
Michael Mason <TeaUponTweed@gmail.com>
2017-03-12 12:45:12 -0600

    Make click produce many squares

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate froggy;
 extern crate ggez;
 extern crate sdl2;
+extern crate rand;
 
 pub mod quadtree;
 pub mod vector;
@@ -8,14 +9,19 @@ pub mod camera;
 pub mod gamestate;
 
 use std::time::Duration;
-
+use std::f32;
+use std::f32::consts::{PI};
 use ggez::conf;
 use ggez::{GameResult, Context};
 use ggez::graphics;
 use ggez::event;
 use ggez::event::{MouseState, EventHandler, Keycode, Mod, MouseButton};
 
+use rand::distributions::{Range, IndependentSample};
+
 use gamestate::{World};
+
+
 
 fn dt_as_float(dt: Duration) -> f32 {
     (dt.as_secs() as f32) +  (dt.subsec_nanos() as f32)/(1000000000.0)
@@ -25,7 +31,9 @@ fn dt_as_float(dt: Duration) -> f32 {
 impl EventHandler for World {
 
     fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
-        self.update_kinematic_entities(dt_as_float(_dt));
+        let dt = dt_as_float(_dt);
+        println!("FPS: {:?}", 1.0/dt);
+        self.update_kinematic_entities(dt);
         Ok(())
     }
 
@@ -37,14 +45,21 @@ impl EventHandler for World {
         Ok(())
     }
 
-    fn mouse_button_down_event(&mut self, _button: MouseButton, _x: i32, _y: i32) {
-        println!("{:?}", "here");
-        println!("{:?}", _x);
-        println!("{:?}", _y);
-        let (x, y) = self.camera.get_real_pos(_x, _y);
-        println!("{:?}", x);
-        println!("{:?}", y);
-        self.add_entity(x, y, 2.0, 2.0);
+    fn mouse_button_down_event(&mut self, _button: MouseButton, mx: i32, my: i32) {
+        let (x, y) = self.camera.get_real_pos(mx, my);
+        let pos_range = Range::new(-100.0, 100.0);
+        let speed_range = Range::new(50.0, 200.0);
+        let dir_range = Range::new(0.0, 2.0*PI);
+        for _ in 0..200 {
+            let dir = dir_range.ind_sample(&mut self.rng);
+            let speed = speed_range.ind_sample(&mut self.rng);
+            let posx = x + pos_range.ind_sample(&mut self.rng);
+            let posy = y + pos_range.ind_sample(&mut self.rng);
+            let velx = speed*dir.cos();
+            let vely = speed*dir.sin();
+            self.add_entity(posx, posy, velx, vely);
+        }
+
     }
 
     fn mouse_wheel_event(&mut self, _x: i32, _y: i32) {
